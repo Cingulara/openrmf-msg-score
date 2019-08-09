d21787098eaad3d98ff3a822a7c93878e23a77fb
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-08-09 17:50:39 -0400

    update to use NATS versus API

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using NATS.Client;
 using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http;
+using System.IO;
+using System.IO.Compression;
 using NLog;
 using NLog.Config;
 using openrmf_msg_score.Models;
 using openrmf_msg_score.Classes;
+using Newtonsoft.Json;
 
 using MongoDB.Bson;
 
@@ -19,8 +20,6 @@ namespace openrmf_msg_score
             LogManager.Configuration = new XmlLoggingConfiguration($"{AppContext.BaseDirectory}nlog.config");
 
             var logger = LogManager.GetLogger("openrmf-msg-score");
-            //logger.Info("log info");
-            //logger.Debug("log debug");
 
             // Create a new connection factory to create a connection.
             ConnectionFactory cf = new ConnectionFactory();
@@ -36,9 +35,10 @@ namespace openrmf_msg_score
                     // print the message
                     logger.Info("New NATS subject: {0}", natsargs.Message.Subject);
                     logger.Info("New NATS data: {0}",Encoding.UTF8.GetString(natsargs.Message.Data));
-                    Artifact checklist = WebClient.GetChecklistAsync(Encoding.UTF8.GetString(natsargs.Message.Data)).GetAwaiter().GetResult();
+                    //Artifact checklist = WebClient.GetChecklistAsync(Encoding.UTF8.GetString(natsargs.Message.Data)).GetAwaiter().GetResult();
+                    Artifact checklist = GetChecklist(c, Encoding.UTF8.GetString(natsargs.Message.Data));
                     if (checklist != null && checklist.CHECKLIST != null) {
-                        Score score = ScoringEngine.ScoreChecklist(checklist.CHECKLIST);
+                        Score score = ScoringEngine.ScoreChecklistString(checklist.rawChecklist);
                         score.system = checklist.system;
                         score.stigType = checklist.stigType;
                         score.stigRelease = checklist.stigRelease;
@@ -62,9 +62,10 @@ namespace openrmf_msg_score
                     // print the message
                     Console.WriteLine(natsargs.Message.Subject);
                     Console.WriteLine(Encoding.UTF8.GetString(natsargs.Message.Data));
-                    Artifact checklist = WebClient.GetChecklistAsync(Encoding.UTF8.GetString(natsargs.Message.Data)).GetAwaiter().GetResult();
+                    //Artifact checklist = WebClient.GetChecklistAsync(Encoding.UTF8.GetString(natsargs.Message.Data)).GetAwaiter().GetResult();
+                    Artifact checklist = GetChecklist(c, Encoding.UTF8.GetString(natsargs.Message.Data));
                     if (checklist != null && checklist.CHECKLIST != null) {
-                        Score score = ScoringEngine.ScoreChecklist(checklist.CHECKLIST);   
+                        Score score = ScoringEngine.ScoreChecklistString(checklist.rawChecklist);   
                         score.system = checklist.system;
                         score.stigType = checklist.stigType;
                         score.stigRelease = checklist.stigRelease;
@@ -112,12 +113,56 @@ namespace openrmf_msg_score
             IAsyncSubscription asyncDelete = c.SubscribeAsync("openrmf.delete", deleteChecklistScore);
             logger.Info("openRMF subscriptions set successfully!");
         }
+
+        // make the string an internal ID for MongoDB
         private static ObjectId GetInternalId(string id)
         {
             ObjectId internalId;
             if (!ObjectId.TryParse(id, out internalId))
                 internalId = ObjectId.Empty;
             return internalId;
+        }
+
+        private static Artifact GetChecklist(IConnection conn, string id){
+            try {
+                Artifact art = new Artifact();
+                Msg reply = conn.Request("openrmf.checklist.read", Encoding.UTF8.GetBytes(id), 30000); // publish to get this Artifact checklist back via ID
+                // save the reply and get back the checklist to score
+                if (reply != null) {
+                    art = JsonConvert.DeserializeObject<Artifact>(DecompressString(Encoding.UTF8.GetString(reply.Data)));
+                    return art;
+                }
+                return art;
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Decompresses the string.
+        /// </summary>
+        /// <param name="compressedText">The compressed text.</param>
+        /// <returns></returns>
+        public static string DecompressString(string compressedText)
+        {
+            byte[] gZipBuffer = Convert.FromBase64String(compressedText);
+            using (var memoryStream = new MemoryStream())
+            {
+                int dataLength = BitConverter.ToInt32(gZipBuffer, 0);
+                memoryStream.Write(gZipBuffer, 4, gZipBuffer.Length - 4);
+
+                var buffer = new byte[dataLength];
+
+                memoryStream.Position = 0;
+                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    gZipStream.Read(buffer, 0, buffer.Length);
+                }
+
+                return Encoding.UTF8.GetString(buffer);
+            }
         }
     }
 }
