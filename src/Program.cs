426b218d09e1b1dce790b228ba1213594557a78a
Dale Bingham <dalebingham@MacBook-Pro.cingularanet>
2020-05-23 11:16:32 -0400

    removing bad check on CHECKLIST

--- src/Program.cs
+++ src/Program.cs
@@ -68,7 +68,7 @@ namespace openrmf_msg_score
                     logger.Info("New NATS subject: {0}", natsargs.Message.Subject);
                     logger.Info("New NATS data: {0}",Encoding.UTF8.GetString(natsargs.Message.Data));
                     Artifact checklist = GetChecklist(c, Encoding.UTF8.GetString(natsargs.Message.Data));
-                    if (checklist != null && checklist.CHECKLIST != null) {
+                    if (checklist != null) {
                         Score score = ScoringEngine.ScoreChecklistString(checklist.rawChecklist);
                         score.systemGroupId = checklist.systemGroupId;
                         score.stigType = checklist.stigType;
@@ -96,7 +96,7 @@ namespace openrmf_msg_score
                     Console.WriteLine(natsargs.Message.Subject);
                     Console.WriteLine(Encoding.UTF8.GetString(natsargs.Message.Data));
                     Artifact checklist = GetChecklist(c, Encoding.UTF8.GetString(natsargs.Message.Data));
-                    if (checklist != null && checklist.CHECKLIST != null) {
+                    if (checklist != null) {
                         Score score = ScoringEngine.ScoreChecklistString(checklist.rawChecklist);   
                         score.systemGroupId = checklist.systemGroupId;
                         score.stigType = checklist.stigType;
